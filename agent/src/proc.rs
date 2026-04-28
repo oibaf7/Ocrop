b96bcf216adae5ff0afa2128fb73a7ff096c637f
Fabio <fabio.cantini.007@gmail.com>
2026-04-28 11:48:39 +0200

    process name and cpu usage

--- agent/src/proc.rs
+++ agent/src/proc.rs
@@ -1,8 +1,10 @@
 use shared::Process;
 use std::error::Error;
-use std::fmt::{Error, format};
+use std::fmt::format;
 use std::fs::{DirEntry, File, ReadDir, read_dir};
-use std::io::{ErrorKind, Read};
+use std::io::Read;
+use std::os::linux::raw::stat;
+use std::os::unix::raw::uid_t;
 use std::path::PathBuf;
 
 const PROC_PATH: &str = "/proc";
@@ -17,20 +19,39 @@ pub fn get_process_ids() -> Result<Vec<u64>, Box<dyn Error>> {
         .collect())
 }
 
-pub fn get_process_details(id: u64) -> Result<Process, Box<dyn Error>> {
+pub fn get_process_details(id: u64, uptime: f64) -> Result<Process, Box<dyn Error>> {
     let base = PathBuf::from(PROC_PATH).join(id.to_string());
-    let status_file = File::open(base.join("status"))?;
-    let stat_file = File::open(base.join("stat"))?;
-    let smaps_rollup_file = File::open(base.join("smaps_rollup"))?;
-    let mut uptime_file = File::open("/proc/uptime")?;
-
-    let uptime: f64 = get_uptime(&mut uptime_file)?;
-
-    Ok(Process::new(0, String::from(""), 0, 0, 0.0, 0))
+    let mut status_file = File::open(base.join("status"))?;
+    let mut stat_file = File::open(base.join("stat"))?;
+    let mut smaps_rollup_file = File::open(base.join("smaps_rollup"))?;
+    let name = get_name(&mut status_file)?;
+    let (ultime, stime, start_time) = get_ultime_and_time(&mut stat_file)?;
+    let cpu_usage = Process::calculate_cpu_usage(ultime, stime, start_time, uptime);
+    
+    Ok(Process::new(id, name, 0, 0, cpu_usage, 0))
 }
 
-fn get_uptime(uptime_file: &mut File) -> Result<f64, Box<dyn Error>> {
+fn get_ultime_and_time(stat_file: &mut File) -> Result<(f64, f64, f64), Box<dyn Error>> {
+    let mut contents = String::from("");
+    stat_file.read_to_string(&mut contents)?;
+    let contents = contents.split_whitespace().collect::<Vec<_>>();
+    let ultime = contents.get(13).unwrap().parse()?;
+    let stime = contents.get(14).unwrap().parse()?;
+    let start_time = contents.get(21).unwrap().parse()?;
+    Ok((ultime, stime, start_time))
+}
+
+fn get_name(status_file: &mut File) -> Result<String, Box<dyn Error>> {
+    let mut content = String::from("");
+    status_file.read_to_string(&mut content)?;
+    let name = content.lines().collect::<Vec<_>>();
+    let name = &name.get(0).unwrap_or(&"Name: Unknown")[6..];
+    Ok(name.to_string())
+}
+
+pub fn get_uptime() -> Result<f64, Box<dyn Error>> {
     let mut uptime_string = String::from("");
+    let mut uptime_file = File::open("/proc/uptime")?;
     uptime_file.read_to_string(&mut uptime_string)?;
     let uptime = uptime_string.split_whitespace().collect::<Vec<_>>();
     let uptime = uptime.get(1).ok_or_else(|| {
--- shared/src/process.rs
+++ shared/src/process.rs
@@ -1,19 +1,19 @@
 pub struct Process {
-    pid: u32, //from folder name
+    pid: u64, //from folder name
     name: String, //from the status file
     rss: u64, //in Kb
     pss: u64, //in Kb
-    cpu: f64,
+    cpu_usage: f64,
     threads: u64
 }
 
 const CLK_TCK: u64 = 100;
 
 impl Process {
-    pub fn new(pid: u32, name: String, rss: u64, pss: u64, cpu: f64, threads: u64) -> Process {
-        Process { pid, name, rss, pss, cpu, threads }
+    pub fn new(pid: u64, name: String, rss: u64, pss: u64, cpu_usage: f64, threads: u64) -> Process {
+        Process { pid, name, rss, pss, cpu_usage, threads }
     }
-    pub fn calculate_cpu_usage(ultime: u64, stime: u64, start_time: u64, uptime: u64) -> f64 {
+    pub fn calculate_cpu_usage(ultime: f64, stime: f64, start_time: f64, uptime: f64) -> f64 {
         let usage: f64 = (ultime as f64 + stime as f64) / CLK_TCK as f64;
         let total_time = (start_time as f64 / CLK_TCK as f64) - uptime as f64;
         usage / total_time
