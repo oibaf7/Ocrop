46f5b456e0d00016fbcbbad33f78ddbc7d6f626c
Fabio <fabio.cantini.007@gmail.com>
2026-05-03 21:55:41 +0200

    setup aggregator skeleton

--- aggregator/src/main.rs
+++ aggregator/src/main.rs
@@ -1,3 +1,20 @@
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    for stream in listener.incoming() {
+        match stream {
+            Ok(s) => {
+                thread::spawn(|| handle_connection(s));
+            },
+            Err(e) => {
+                println!("Error while receiving stream. Error: {e}");
+            }
+        }
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    //send to frontend
 }
